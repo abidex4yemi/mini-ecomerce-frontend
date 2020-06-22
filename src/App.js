c547869af160d46223bd0cb0a1fabf6b3b9d6274
abidex4yemi <abidex4yemi@gmail.com>
2020-06-22 15:20:39 +0100

    chore: update readme

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-// import Products from './pages/Products/Products';
-// import AddProduct from './pages/Products/AddProduct';
-// import Header from './components/molecules/Header';
-// import Signup from './pages/Signup';
-// import { Login } from './pages/Login';
-// import { GlobalStyles } from './components/atom';
-// import requiresAuth from './components/HOC/requireAuth';
+import Products from './pages/Products/Products';
+import AddProduct from './pages/Products/AddProduct';
+import Header from './components/molecules/Header';
+import Signup from './pages/Signup';
+import { Login } from './pages/Login';
+import { GlobalStyles } from './components/atom';
+import requiresAuth from './components/HOC/requireAuth';
 
 const App = () => {
   return (
-    <div>Welcome</div>
-    // <>
-    //   <Header />
-    //   <GlobalStyles />
-    //   <Switch>
-    //     <Route exact path="/signup" component={Signup} />
-    //     <Route exact path="/login" component={Login} />
-    //     <Route exact path="/" component={Products} />
-    //     <Route exact path="/add-product" component={requiresAuth(AddProduct)} />
-    //   </Switch>
-    // </div>
+    <>
+      <Header />
+      <GlobalStyles />
+      <Switch>
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Products} />
+        <Route exact path="/add-product" component={requiresAuth(AddProduct)} />
+      </Switch>
+    </>
   );
 };
 
